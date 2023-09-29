73177583bc3f25a4f7f3f71ff426f2c94b3118f4
pushpamohankumar <heshikagowda5@gmail.com>
2023-09-29 07:14:01 +0530

    New Register

---
+++ src/test/java/HomeModule/NewRegister_user.java
@@ -0,0 +1,10 @@
+package HomeModule;
+
+import org.testng.annotations.Test;
+
+public class NewRegister_user {
+@Test
+public void newUserRegistration() {
+	System.out.println("Login as a New User");
+}
+}
